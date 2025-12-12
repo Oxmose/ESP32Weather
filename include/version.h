aa4b0fcf86a295c7acba78d1612fee66b65a1a5a
oxmose <alexy.torresa@gmail.com>
2025-12-12 18:07:00 +0100

    Added versioning system

---
+++ include/version.h
@@ -0,0 +1,11 @@
+
+    #ifndef BUILD_NUMBER
+        #define BUILD_NUMBER "7"
+    #endif
+    #ifndef VERSION
+        #define VERSION "0.1.7 2025-12-12 18:06:32"
+    #endif
+    #ifndef VERSION_SHORT
+        #define VERSION_SHORT "0.1.7"
+    #endif
+    
