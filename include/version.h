ad673b057654da0fb3ea098ebf8244f482d2be88
oxmose <alexy.torresa@gmail.com>
2026-01-02 18:25:58 +0100

    Updated time function to use internal timer in ms

--- include/version.h
+++ include/version.h
@@ -1,11 +1,11 @@
 
     #ifndef BUILD_NUMBER
-        #define BUILD_NUMBER "722"
+        #define BUILD_NUMBER "728"
     #endif
     #ifndef VERSION
-        #define VERSION "0.1.722 2026-01-02 17:51:21"
+        #define VERSION "0.1.728 2026-01-02 18:24:29"
     #endif
     #ifndef VERSION_SHORT
-        #define VERSION_SHORT "0.1.722"
+        #define VERSION_SHORT "0.1.728"
     #endif
     
